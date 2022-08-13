1320a015762588020a6d559c7a17e94e04c7e1d4
ysmngngr <gngryasemin16@gmail.com>
2022-08-13 21:55:10 +0300

    2.C

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -1,6 +1,7 @@
 public class Day1Git {
     public static void main(String[] args) {
         System.out.println("Hello Git 1. Deneme");
+        System.out.println("Hello Git 1. Deneme");
     }
 
 
